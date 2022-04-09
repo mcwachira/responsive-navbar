c1844b84baba700c733ba1a9cfa2ed2ac8fbf208
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-04-09 10:04:09 +0300

    added a dropdwon to the Navigation menui

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const dropDown = document.querySelector('.drop-down ');
+const dropDownMenu = document.querySelector('.drop-down-menu')
+
+
+//target the event
+dropDown.addEventListener('click', (e) => {
+
+    dropDownMenu.classList.toggle('drop-down-display')
+}
+
+)
+console.log('hello')
