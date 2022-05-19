97d48216886a7904ae12e30b3576444388693300
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-05-19 10:43:19 +0300

    just another mobile first navbar

--- js/app.js
+++ js/app.js
@@ -1,12 +1,51 @@
-const dropDown = document.querySelector('.drop-down ');
-const dropDownMenu = document.querySelector('.drop-down-menu')
+const toggle = document.querySelector('.toggle')
+const menu = document.querySelector('.menu')
+
+// Toggle mobile menu
+
+const toggleMenu = () => {
+    if (menu.classList.contains('active')) {
+        menu.classList.remove('active')
 
 
-//target the event
-dropDown.addEventListener('click', (e) => {
+        //add menu hamburger icon
+        toggle.querySelector('a').innerHTML = "<i class='fas fa - bars > </i >"
+    } else {
+        menu.classList.add('active');
 
-    dropDownMenu.classList.toggle('drop-down-display')
+        //add menu close  icon
+        toggle.querySelector('a').innerHTML = "<i class='fas fa - times > </i >"
+
+    }
 }
 
-)
-console.log('hello')
+// Event listener
+
+toggle.addEventListener('click', toggleMenu, false)
+
+//submenu 
+
+const items = document.querySelectorAll('.item');
+
+function toggleSubMenu() {
+    if (this.classList.contains("submenu-active")) {
+        this.classList.remove("submenu-active");
+
+    } else {
+        if (menu.querySelector(".submenu.active")) {
+            menu.querySelector(".submenu-active").classList.remove("submenu-active")
+            this.classList.add('submenu-active')
+        } else {
+            this.classList.add('submenu-active')
+        }
+    }
+}
+
+// Event listeners
+
+for (let item of items) {
+    if (item.querySelector(".submenu")) {
+        item.addEventListener('click', toggleSubMenu, false);
+        item.addEventListener('keypress', toggleSubMenu, false)
+    }
+}
